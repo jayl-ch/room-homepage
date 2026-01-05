d610e2cc34e67cf85103f3fd860198d8c1ec8ef1
jayl-ch <jaychicogo328@gmail.com>
2026-01-06 00:48:04 +0800

    fix: typo in homepage card paragraph

--- src/components/MainContent/Discover.jsx
+++ src/components/MainContent/Discover.jsx
@@ -109,7 +109,7 @@ const Description = ({ index }) => {
     {
       title: "Manufactured with the best materials",
       paragraph:
-        "Our modern furniture store provide high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. WIth three decades of experience in this undustry, we understand what customers want for their home and office.",
+        "Our modern furniture store provide high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
     },
   ];
 
