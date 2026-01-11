857081136d50e21d36558a14547cdd1bb5480f41
jayl-ch <jaychicogo328@gmail.com>
2026-01-11 13:43:30 +0800

    Added opacity animation to card_description

--- src/components/MainContent/Discover.jsx
+++ src/components/MainContent/Discover.jsx
@@ -97,16 +97,19 @@ const ShopNow = () => (
 const Description = ({ index }) => {
   const card = [
     {
+      id: 1,
       title: "Discover innovative ways to decorate",
       paragraph:
         "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
     },
     {
+      id: 2,
       title: "We are available all across the globe",
       paragraph:
         "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     },
     {
+      id: 3,
       title: "Manufactured with the best materials",
       paragraph:
         "Our modern furniture store provide high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
@@ -114,9 +117,12 @@ const Description = ({ index }) => {
   ];
 
   return (
-    <div className="home__discover-description c-card-description">
-      <h2 className="home__discover-title">{card[index].title}</h2>
-      <p className="home__discover-paragraph c-card-paragraph">
+    <div
+      key={card[index].id}
+      className="home__discover-description c-card-description"
+    >
+      <h2 className="home__discover-title animate">{card[index].title}</h2>
+      <p className="home__discover-paragraph c-card-paragraph animate">
         {card[index].paragraph}
       </p>
       <ShopNow />
